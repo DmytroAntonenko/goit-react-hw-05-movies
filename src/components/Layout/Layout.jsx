171423b5778c6e2aa43a58840c89854f4061acff
DmytroAntonenko <d.antonenko81@gmail.com>
2023-01-09 14:32:50 +0200

    2

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+import Navigation from './Navigation';
+
+const Layout = () => (
+  <header className="Header">
+    <Navigation />
+  </header>
+);
+
+export default Layout;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import routes from '../../routes';
+
+const Navigation = () => (
+  <nav className="Navigation">
+    <NavLink
+      exact
+      to={routes.home}
+      className="Home"
+      activeClassName="ActiveLinkNav"
+    >
+      Home
+    </NavLink>
+    <NavLink to={routes.movies} activeClassName="ActiveLinkNav">
+      Movies
+    </NavLink>
+  </nav>
+);
+
+export default Navigation;
